aa16c982d2215e2fd5e0362fa0cba15525504f07
Hubcio <hubert.firek@o2.pl>
2019-11-16 10:00:30 +0100

    spirng komit 1

---
+++ src/main/java/pl/hubf/git_examples/springprzyklad/SpringprzykladApplication.java
@@ -0,0 +1,14 @@
+package pl.hubf.git_examples.springprzyklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprzykladApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringprzykladApplication.class, args);
+    }
+
+
+}
